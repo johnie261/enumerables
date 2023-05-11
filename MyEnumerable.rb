37719a8eabc93af8c2a26a3bbe1b7b8ec3383301
johnie261 <njorogejohn261@gmail.com>
2023-05-11 23:03:40 +0300

    Fix linters

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
-    def all?(&block)
-      each { |elem| return false unless block.call(elem) }
-      true
-    end
-
-    def any?(&block)
-      each { |elem| return true if block.call(elem) }
-      false
-    end
-
-    def filter(&block)
-      result = []
-      each { |elem| result << elem if block.call(elem) }
-      result
-    end
+  def all?(&block)
+    each { |elem| return false unless block.call(elem) }
+    true
   end
+
+  def any?(&block)
+    each { |elem| return true if block.call(elem) }
+    false
+  end
+
+  def filter(&block)
+    result = []
+    each { |elem| result << elem if block.call(elem) }
+    result
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -1,33 +1,33 @@
 require_relative 'MyEnumerable'
 
 class MyList
-    include MyEnumerable
-    include Enumerable
+  include MyEnumerable
+  include Enumerable
 
-    attr_reader :list
+  attr_reader :list
 
-    def initialize(*args)
-        @list = args
-    end
+  def initialize(*args)
+    @list = args
+  end
 
-    def each(&block)
-        @list.each(&block)
-    end
+  def each(&block)
+    @list.each(&block)
+  end
 end
 
 list = MyList.new(1, 2, 3, 4)
 
-results = list.all? {|e| e < 5}
+results = list.all? { |e| e < 5 }
 puts results
 
-results = list.all? {|e| e > 5}
+results = list.all? { |e| e > 5 }
 puts results
 
-results = list.any? {|e| e == 2}
+results = list.any? { |e| e == 2 }
 puts results
 
-results = list.any? {|e| e == 5}
+results = list.any? { |e| e == 5 }
 puts results
 
-results = list.filter {|e| e.even?}
+results = list.filter { |e| e.even? }
 puts results
